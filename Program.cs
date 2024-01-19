a45f40b8829d2a4bf447123cfa880e3dbec178c8
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-19 16:12:43 +0530

    [Ajay]: add uc2

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace snake_and_ladder
 {
@@ -10,8 +11,23 @@ namespace snake_and_ladder
     {
         static void Main(string[] args)
         {
-            int player1 = 0; // this is inital position
+            int player1 = 0;
+            /*List<int> v = new List<int>();
+            v.Add(1);
+            v.Add(2);
+            foreach(int i in v)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();*/
 
+            Random rand = new Random();
+            int random_number = rand.Next(6);
+            random_number++;
+            Console.Write("The number on dice is : ");
+            Console.WriteLine(random_number);
+
+            Console.ReadLine();
         }
     }
 }
