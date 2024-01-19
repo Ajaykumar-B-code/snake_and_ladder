fa362b178e4724bca6ad9327864b9e12e725973d
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-19 15:07:43 +0530

    [Ajay] add: use case 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_and_ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int player1 = 0; // this is inital position
+
+        }
+    }
+}
