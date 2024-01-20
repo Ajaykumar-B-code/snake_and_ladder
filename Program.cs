b7dfa9ffcfa835e6c37d99ad6c06f7bf6412ade0
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-20 10:55:36 +0530

    [Ajay] Add: uc3

--- Program.cs
+++ Program.cs
@@ -9,25 +9,42 @@ namespace snake_and_ladder
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
             int player1 = 0;
-            /*List<int> v = new List<int>();
-            v.Add(1);
-            v.Add(2);
-            foreach(int i in v)
-            {
-                Console.WriteLine(i);
-            }
-            Console.ReadLine();*/
-
             Random rand = new Random();
             int random_number = rand.Next(6);
             random_number++;
-            Console.Write("The number on dice is : ");
-            Console.WriteLine(random_number);
-
+            Console.WriteLine($"The number on the dice is {random_number}");
+            int option = rand.Next(3);
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("No move");
+                    break;
+                case 1:
+                    Console.WriteLine("It is a ladder");
+                    player1 += random_number;
+                    break;
+                case 2:
+                    Console.WriteLine("It is a snake");
+                    if (random_number > player1)
+                    {
+                        player1 = 0;
+                    }
+                    else
+                    {
+                        player1 -= random_number;
+                    }
+                    break;
+            }
+            Console.Write("player postiton is : ");
+            Console.WriteLine(player1);
             Console.ReadLine();
+            
+            
+      
         }
     }
 }
