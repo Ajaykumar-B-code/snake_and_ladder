b413c333e321aca911dfdfb238f5e575b6d2a5c4
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-20 11:22:08 +0530

    [Ajay] Add: uc4

--- Program.cs
+++ Program.cs
@@ -14,37 +14,43 @@ namespace snake_and_ladder
         {
             int player1 = 0;
             Random rand = new Random();
-            int random_number = rand.Next(6);
-            random_number++;
-            Console.WriteLine($"The number on the dice is {random_number}");
-            int option = rand.Next(3);
-            switch (option)
+            do
             {
-                case 0:
-                    Console.WriteLine("No move");
-                    break;
-                case 1:
-                    Console.WriteLine("It is a ladder");
-                    player1 += random_number;
-                    break;
-                case 2:
-                    Console.WriteLine("It is a snake");
-                    if (random_number > player1)
-                    {
-                        player1 = 0;
-                    }
-                    else
-                    {
-                        player1 -= random_number;
-                    }
-                    break;
+                int random_number = rand.Next(6);
+                random_number++;
+                Console.WriteLine($"The number on the dice is {random_number}");
+                int option = rand.Next(3);
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No move");
+                        break;
+                    case 1:
+                        Console.WriteLine("It is a ladder");
+                        player1 += random_number;
+                        break;
+                    case 2:
+                        Console.WriteLine("It is a snake");
+                        if (random_number > player1)
+                        {
+                            Console.WriteLine("In this case player moves below 0, player go to intial position");     
+                            player1 = 0;
+                        }
+                        else
+                        {
+                            player1 -= random_number;
+                        }
+                        break;
+                }
+                
+               
+                    Console.Write("player postiton is : ");
+                    Console.WriteLine(player1);
+                    Console.WriteLine("\n");
+                
             }
-            Console.Write("player postiton is : ");
-            Console.WriteLine(player1);
+            while (player1 <= 100);
             Console.ReadLine();
-            
-            
-      
         }
     }
 }
